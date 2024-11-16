cf25ac7ff31b2c39f1ca219103db9b274479a897
Soujanya Nayak <soujanya4013@gmail.com>
2024-11-16 21:14:14 +0530

    Some Frontend Changes

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './HomePage.css';
+import backgroundImage from '../assets/BG.jpg';
 
 const HomePage = () => {
   const [fasta, setFasta] = useState('');
@@ -12,7 +13,7 @@ const HomePage = () => {
   const [responseText, setResponseText] = useState('');
   const [treeHtmlUrl, setTreeHtmlUrl] = useState('');
   const [blastResultUrl, setBlastResultUrl] = useState('');
-  const [topBlastResults, setTopBlastResults] = useState([]); // State for top 10 BLAST results
+  const [topBlastResults, setTopBlastResults] = useState([]);
 
   const handleFindClick = async () => {
     setLoading(true);
@@ -46,7 +47,7 @@ const HomePage = () => {
         setResponseText(data.response);
         setTreeHtmlUrl(`http://localhost:5000${data.tree_image_url}`);
         setBlastResultUrl(`http://localhost:5000${data.file_url}`);
-        setTopBlastResults(data.top_hits || []); // Set top 10 BLAST results
+        setTopBlastResults(data.top_hits || []);
       } else {
         setError(`Error: ${data.error}`);
       }
@@ -75,82 +76,93 @@ const HomePage = () => {
   }, [loading]);
 
   return (
-    <div className="homepage-container">
-      <h1 className="homepage-title">Ascension Number Search Tool</h1>
-      <div className="form-container">
-        <input
-          className="fasta-input"
-          type="text"
-          placeholder="Enter Ascension Number"
-          value={fasta}
-          onChange={(e) => setFasta(e.target.value)}
-        />
-        <select className="select-blast-type" value={blastType} onChange={(e) => setBlastType(e.target.value)}>
-          <option value="blastn">BLASTN (Nucleotide)</option>
-          <option value="blastp">BLASTP (Protein)</option>
-          <option value="blastx">BLASTX (Translated nucleotide to protein)</option>
-          <option value="tblastn">TBLASTN (Protein to translated nucleotide)</option>
-          <option value="tblastx">TBLASTX (Translated nucleotide to translated nucleotide)</option>
-        </select>
-        <select className="select-database" value={database} onChange={(e) => setDatabase(e.target.value)}>
-          <option value="nt">Nucleotide collection (nt)</option>
-          <option value="nr">Non-redundant protein (nr)</option>
-          <option value="refseq_rna">RefSeq RNA</option>
-          <option value="refseq_protein">RefSeq Protein</option>
-          <option value="swissprot">SwissProt (Manually curated protein sequences)</option>
-          <option value="core_nt">Core Nucleotide Database (core_nt)</option>
-        </select>
-        <button className="find-button" onClick={handleFindClick} disabled={loading}>
-          {loading ? 'Finding...' : 'Find'}
-        </button>
+    <div
+      className="homepage-container"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="content-box">
+        <h1 className="homepage-title">Ascension Number Search Tool</h1>
+        <div className="form-container">
+          <input
+            className="fasta-input"
+            type="text"
+            placeholder="Enter Ascension Number"
+            value={fasta}
+            onChange={(e) => setFasta(e.target.value)}
+          />
+          <select className="select-blast-type" value={blastType} onChange={(e) => setBlastType(e.target.value)}>
+            <option value="blastn">BLASTN (Nucleotide)</option>
+            <option value="blastp">BLASTP (Protein)</option>
+            <option value="blastx">BLASTX (Translated nucleotide to protein)</option>
+            <option value="tblastn">TBLASTN (Protein to translated nucleotide)</option>
+            <option value="tblastx">TBLASTX (Translated nucleotide to translated nucleotide)</option>
+          </select>
+          <select className="select-database" value={database} onChange={(e) => setDatabase(e.target.value)}>
+            <option value="nt">Nucleotide collection (nt)</option>
+            <option value="nr">Non-redundant protein (nr)</option>
+            <option value="refseq_rna">RefSeq RNA</option>
+            <option value="refseq_protein">RefSeq Protein</option>
+            <option value="swissprot">SwissProt (Manually curated protein sequences)</option>
+            <option value="core_nt">Core Nucleotide Database (core_nt)</option>
+          </select>
+          <button className="find-button" onClick={handleFindClick} disabled={loading}>
+            {loading ? 'Finding...' : 'Find'}
+          </button>
+        </div>
+
+        <div className="status-container">
+          <p>Status: {status}</p>
+          <p>Elapsed Time: {timer}s</p>
+        </div>
+
+        {error && <div className="error-container"><p>{error}</p></div>}
+
+        {treeHtmlUrl && (
+          <div className="tree-container">
+            <div className="xyz">
+              <iframe
+                src={treeHtmlUrl}
+                title="Phylogenetic Tree"
+                className="tree-iframe"
+                frameBorder="0"
+              ></iframe>
+            </div>
+          </div>
+        )}
+
+        {responseText && (
+          <div className="response-container">
+            <h2>Source of Collection:</h2>
+            <p>{responseText}</p>
+          </div>
+        )}
+
+        {topBlastResults.length > 0 && (
+          <div className="top-blast-results-container">
+            <div className="best-match-container">
+              <h2>Best Match:</h2>
+              <p>{topBlastResults[0].title}</p>
+              <p><strong>GenBank Submission:</strong> <a href={topBlastResults[0].publicationLink} target="_blank" rel="noopener noreferrer">{topBlastResults[0].publicationLink}</a></p>
+            </div>
+            <div className="other-matches-container">
+              <h2>Other Top Matches:</h2>
+              {topBlastResults.slice(1, 9).map((result, index) => (
+                <div key={index + 1} className="blast-result-item">
+                  <p><strong>{index + 2}.</strong> <strong>Title:</strong> {result.title}</p>
+                  <p><strong>GenBank Submission:</strong> <a href={result.publicationLink} target="_blank" rel="noopener noreferrer">{result.publicationLink}</a></p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+
+        {blastResultUrl && (
+          <div className="blast-result-container">
+            <h2>Full BLAST Result:</h2>
+            <a href={blastResultUrl} target="_blank" rel="noopener noreferrer">View Full BLAST Result</a>
+          </div>
+        )}
       </div>
-
-      <div className="status-container">
-        <p>Status: {status}</p>
-        <p>Elapsed Time: {timer}s</p>
-      </div>
-
-      {error && <div className="error-container"><p>{error}</p></div>}
-
-      {responseText && (
-        <div className="response-container">
-          <h2>Source of Collection:</h2>
-          <p>{responseText}</p>
-        </div>
-      )}
-
-      {topBlastResults.length > 0 && (
-        <div className="top-blast-results-container">
-          <h2>Top 10 BLAST Results:</h2>
-          <ul>
-            {topBlastResults.map((result, index) => (
-              <li key={index} className="blast-result-item">
-                <p><strong>Title:</strong> {result.title}</p>
-                <p><strong>Author/Publication Link:</strong> <a href={result.publicationLink} target="_blank" rel="noopener noreferrer">{result.publicationLink}</a></p>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-
-      {treeHtmlUrl && (
-        <div className="tree-container">
-          <h2>Phylogenetic Tree:</h2>
-          <iframe
-            src={treeHtmlUrl}
-            title="Phylogenetic Tree"
-            className="tree-iframe"
-            frameBorder="0"
-          ></iframe>
-        </div>
-      )}
-
-      {blastResultUrl && (
-        <div className="blast-result-container">
-          <h2>Full BLAST Result:</h2>
-          <a href={blastResultUrl} target="_blank" rel="noopener noreferrer">View Full BLAST Result</a>
-        </div>
-      )}
     </div>
   );
 };
